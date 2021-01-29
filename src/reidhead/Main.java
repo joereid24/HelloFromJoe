412bba248bc0878161caedadf22787f65bb7daaa
Joseph Reidhead <78058167+joereid24@users.noreply.github.com>
2021-01-28 19:49:15 -0700

    Initial commit

---
+++ src/reidhead/Main.java
@@ -0,0 +1,15 @@
+package reidhead;
+
+import java.util.Scanner;
+
+public class Main {
+    private static final Scanner userInput = new Scanner(System.in);
+    public static void main(String[] args) {
+        String UserName;
+
+        System.out.println("Please input your name: ");
+        UserName = userInput.nextLine();
+
+        System.out.println("Hello "+UserName+" welcome to the new you!");
+    }
+}
